222d00fda2cc407d392ccd266513efea865988be
Stefano <stefano2890@gmail.com>
2020-06-17 16:49:48 +0200

    a little style

--- index.php
+++ index.php
@@ -4,17 +4,20 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title></title>
+        <link rel="stylesheet" href="public/style.css">
+        <title>Dischi PHP</title>
     </head>
     <body>
-        <?php foreach ($dischi as $value) { ?>
-            <div class="scheda">
-                <img src="<?php echo $value['poster'] ?>" alt="<?php echo $value['title'] ?>">
-                <h1><?php echo $value['title'] ?></h1>
-                <h2><?php echo $value['author'] ?></h2>
-                <p><?php echo $value['year'] ?></p>
-                <p><?php echo $value['genre'] ?></p>
-            </div>
-        <?php } ?>
+        <div class="container playlist">
+            <?php foreach ($dischi as $value) { ?>
+                <div class="scheda">
+                    <img src="<?php echo $value['poster'] ?>" alt="<?php echo $value['title'] ?>">
+                    <h1><?php echo $value['title'] ?></h1>
+                    <h2><?php echo $value['author'] ?></h2>
+                    <p><?php echo $value['year'] ?></p>
+                    <p><?php echo $value['genre'] ?></p>
+                </div>
+            <?php } ?>
+        </div>
     </body>
 </html>
