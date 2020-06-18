c3f27788e522c17718851a1f70b27800b3538fed
Stefano <stefano2890@gmail.com>
2020-06-18 12:14:34 +0200

    creata funzione per filtro artista ma solo lato frontend

--- versione-ajax/main.js
+++ versione-ajax/main.js
@@ -1,19 +1,28 @@
 
 $(document).ready(function() {
 
-    // faccio partire la chiamata ajax al mio file dischi.php da cui ho convertito un array in oggetto json;
-    $.ajax({
-        'url': 'db/dischi.php',
-        'method': 'GET',
-        'success': function(dischi) {
+    // richiamo la funzione per la chiamata generale;
+    chiamata();
 
-            // richiamo la funzione per ciclare i dischi
-            cicla_dischi(dischi);
-        },
-        'error': function() {
-            console.log('si è verificato un errore');
-        }
-    });
+    // richiamo la funzione per filtrare in base all'artista selezionato;
+    filra_artist()
+
+    // faccio partire la chiamata ajax al mio file dischi.php da cui ho convertito un array in oggetto json;
+    function chiamata() {
+        $.ajax({
+            'url': 'db/dischi.php',
+            'method': 'GET',
+            'success': function(dischi) {
+
+                // richiamo la funzione per ciclare i dischi
+                cicla_dischi(dischi);
+            },
+            'error': function() {
+                console.log('si è verificato un errore');
+            }
+        });
+    }
+
 
     // creo la funzione per ciclare i dischi;
     function cicla_dischi(dischi) {
@@ -46,6 +55,7 @@ $(document).ready(function() {
         $('.playlist').append(html);
     }
 
+    // creo la funzione per stampare le options della select con handlebars;
     function stampa_option(artist) {
 
         // dichiaro le variabili per integrare handlebars;
@@ -57,8 +67,44 @@ $(document).ready(function() {
         }
         var html  = template(context);
 
-        // appendo il tutto nel contenitore con classe playlist;
+        // appendo il tutto nella select;
         $('.artist').append(html);
 
     }
+
+    // creo funzione per filtrare per artista;
+    function filra_artist() {
+        $('.artist').change(function () {
+
+               // dichiaro una variabile che rappresenta il singolo elemento nell'elemento select;
+               var artist = $(this).val();
+
+               console.log(artist);
+
+               // nascondi tutte le schede;
+
+               $('.scheda').hide();
+
+               $.ajax({
+                   'url': 'db/dischi.php',
+                   'method': 'GET',
+                   'success': function(dischi) {
+                       for (var i = 0; i < dischi.length; i++) {
+                           var disk = dischi[i]
+                           // se seleziono 'artist' mostro tutte le schede;
+                           if (artist == disk.author) {
+                               stampa_generale(disk)
+                           } else if (artist == 'artist') {
+                               stampa_generale(disk)
+                           }
+
+                       }
+                   },
+                   'error': function() {
+                       console.log('si è verificato un errore');
+                   }
+               });
+           });
+    }
+
 });
